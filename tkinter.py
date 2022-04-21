706fa6334625d6ffd14c68be29217f531d8d8822
Erwann-M <rwnnmrtn@gmail.com>
2022-04-21 10:28:04 +0200

    fix bug TypeError

--- tkinter.py
+++ tkinter.py
@@ -4,9 +4,12 @@ class TkinterTools:
     def __init__(self):
         pass
     
-    def center_window(self, main_app, window_x, window_y):
+    def center_window(cls, main_app, window_x, window_y):
         screen_x = int(main_app.winfo_screenwidth())
         screen_y = int(main_app.winfo_screenheight())
+        
+        window_x = int(window_x)
+        window_y = int(window_y)
 
         posX = int((screen_x / 2) - (window_x / 2))
         posY = int((screen_y / 2) - (window_y / 2))
@@ -14,4 +17,6 @@ class TkinterTools:
         geo = "{}x{}+{}+{}".format(window_x, window_y, posX, posY)
         
         return geo
+    
+    center_window = classmethod(center_window)
         
